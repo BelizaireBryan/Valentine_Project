fe955afd163c7f7d0da88bdd3041c465c69c8ae3
BelizaireBryan <159981354+BelizaireBryan@users.noreply.github.com>
2024-02-13 23:06:44 -0500

    Add files via upload

---
+++ ideas.js
@@ -0,0 +1,13 @@
+let y1_btn = document.getElementById("yes1");
+let y2_btn = document.getElementById("yes2");
+
+y1_btn.onclick = function (event)
+{
+    alert("So glad you made the right choice :)" +
+        " Thank you bookie")
+}
+
+y2_btn.onclick = function (event)
+{
+    alert("Oh my goodness, I'm so honored, thank you my love")
+}
